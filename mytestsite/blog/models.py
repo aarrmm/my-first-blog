17bb98e70c5e4676480ccc812504518174d06da2
aarrmm <sunsetar1126@gmail.com>
2021-08-26 18:32:15 +0900

    Modified template to display posts from DB.

--- mytestsite/blog/models.py
+++ mytestsite/blog/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+
 # 모델(객체) 정의 코드
 # models는 Post가 장고모델임을 의미하여 DB에 저장하는 것을 인식
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,\
-         on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
         default=timezone.now)
-    
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
     def __str__(self):
         return self.title
-
 
 # models.CharField(글자수) : 글자수가 제한된 text를 정의할 때 사용.
 #                            글제목처럼 짧은 문자열 정보저장
@@ -25,4 +29,3 @@ class Post(models.Model):
 # 공식문서 참조 https://docs.djangoproject.com/en/3.2/ref/models/fields/ 
 
 # 생성된 데이터를 DB에 반영(makemigrations)
-
--- mytestsite/blog/views.py
+++ mytestsite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-# 요청을 받아서 render라는 메소드를 호출하고 post_list.html을 보여줌
-
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
